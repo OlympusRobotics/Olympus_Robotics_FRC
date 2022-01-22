a3e130b2ae61b03bb482cf7525c0179657041b30
Robotics Computer <billybobjoethe2nd@aol.com>
2022-01-22 08:03:50 -0500

    fixed imports

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,30 +1,32 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+ /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+         /* Open Source Software - may be modified and shared by FRC teams. The code   */
+         /* must be accompanied by the FIRST BSD license file in the root directory of */
+         /* the project.                                                               */
+         /*----------------------------------------------------------------------------*/
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.controls.DriveControlBase;
-import frc.robot.controls.tankdrive.ArcadeControls;
-import frc.robot.controls.tankdrive.CurvatureControls;
-import frc.robot.controls.tankdrive.TankControls;
-import frc.robot.hardware.HardwareMap;
-import frc.robot.hardware.HardwareMapEx;
+ import edu.wpi.first.wpilibj.GenericHID;
+ import edu.wpi.first.wpilibj.IterativeRobot;
+ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+ import frc.robot.controls.DriveControlBase;
+ import frc.robot.controls.tankdrive.ArcadeControls;
+ import frc.robot.controls.tankdrive.CurvatureControls;
+ import frc.robot.controls.tankdrive.TankControls;
+ import frc.robot.hardware.HardwareMap;
+ import frc.robot.hardware.HardwareMapEx;
 
-import java.util.HashMap;
-import java.util.Map;
+ import java.util.HashMap;
+ import java.util.Map;
+
+
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
+ *  * The VM is configured to automatically run this class, and to call the
+ *  * functions corresponding to each mode, as described in the IterativeRobot
+ *  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the build.gradle file in the
  * project.
  */
