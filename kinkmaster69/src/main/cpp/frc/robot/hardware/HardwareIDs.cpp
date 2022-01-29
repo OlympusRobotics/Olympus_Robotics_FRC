4aad652a9c296584f060ec8cdfed750a3a34ab18
Abdullah Mohammed <aamohammed4556@gmail.com>
2022-01-29 12:13:36 -0500

    Added hardwareids enum and hardwaremap

---
+++ kinkmaster69/src/main/cpp/frc/robot/hardware/HardwareIDs.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+
+enum HardWareIDs {
+    
+    frontLeftMotor = 0,
+    frontRightMotor = 1,
+    rearLeftMotor = 2,
+    rearRightMotor = 3,
+    intakeMotor = 4,
+    outputMotor1 = 5,
+    outputMotor2 = 6,
+    beltMotor = 7
+
+};
--- kinkmaster69/src/main/cpp/frc/robot/hardware/HardwareMap.cpp
+++ kinkmaster69/src/main/cpp/frc/robot/hardware/HardwareMap.cpp
@@ -2,15 +2,16 @@
 #include <frc/Solenoid.h>
 #include <frc/Joystick.h>
 #include "ctre/Phoenix.h"
+#include "HardwareIDs.cpp"
 
 class HardwareMap {
     public:
-        TalonSRX intakeMotor = {4};
-        TalonSRX outputMotor1 = {5};
-        TalonSRX outputMotor2 = {6};
-        TalonSRX beltMotor = {7};
-        VictorSPX frontLeftMotor = {0};
-        VictorSPX frontRightMotor = {1};
-        VictorSPX rearLeftMotor = {2};
-        VictorSPX rearRightMotor = {3};
+        TalonSRX intakeMotor = {HardWareIDs::intakeMotor};
+        TalonSRX outputMotor1 = {HardWareIDs::outputMotor1};
+        TalonSRX outputMotor2 = {HardWareIDs::outputMotor2};
+        TalonSRX beltMotor = {HardWareIDs::beltMotor};
+        VictorSPX frontLeftMotor = {HardWareIDs::frontLeftMotor};
+        VictorSPX frontRightMotor = {HardWareIDs::frontRightMotor};
+        VictorSPX rearLeftMotor = {HardWareIDs::rearLeftMotor};
+        VictorSPX rearRightMotor = {HardWareIDs::rearRightMotor};
 };
