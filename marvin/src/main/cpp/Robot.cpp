15afe3076511c27935f7265dc156c3f67dfbe66c
Olympus Robotics <jason.wang@students.sacs.k12.in.us>
2022-02-26 09:51:04 -0500

    changed formatting for readability added comments added drift mode

--- marvin/src/main/cpp/Robot.cpp
+++ marvin/src/main/cpp/Robot.cpp
@@ -12,6 +12,7 @@
 #include "ctre/phoenix/motorcontrol/ControlMode.h"
 
 HardwareMap hw;
+bool cMode = false; //drifting - called cMode for cameron's mode
 
 void Robot::RobotInit() {
   m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
@@ -22,56 +23,59 @@ void Robot::RobotInit() {
 
 void mainPeriodic(){
   
-  //forward/backward on joysticks
-  //double joystickY = hw.leftJoystick.GetY() < 0 || hw.rightJoystick.GetY() < 0 ? std::min(hw.leftJoystick.GetY(), hw.rightJoystick.GetY()) : std::max(hw.leftJoystick.GetY(), hw.rightJoystick.GetY());
-
+  //this makes it so you can use either joystick - just takes the most extreme one
+  //forward = +, backward = -
   double joystickY;
-  if (hw.leftJoystick.GetY() < 0 || hw.rightJoystick.GetY() < 0){
-    joystickY = std::min(hw.leftJoystick.GetY(), hw.rightJoystick.GetY());
-  } else {
-    joystickY = std::max(hw.leftJoystick.GetY(), hw.rightJoystick.GetY());
-  }
+  if (hw.leftJoystick.GetY() < 0 || hw.rightJoystick.GetY() < 0) joystickY = std::min(hw.leftJoystick.GetY(), hw.rightJoystick.GetY());
+  else joystickY = std::max(hw.leftJoystick.GetY(), hw.rightJoystick.GetY());
 
-  double turningSensitivity = 0.128;
+  //right = +, left = -
+  double joystickX;
+  if(hw.leftJoystick.GetX() < 0 || hw.rightJoystick.GetX() < 0) joystickX = std::min(hw.leftJoystick.GetX(), hw.rightJoystick.GetX());
+  else joystickX = std::max(hw.leftJoystick.GetX(), hw.rightJoystick.GetX());
 
-  double joystickX = hw.leftJoystick.GetX() < 0 || hw.rightJoystick.GetX() < 0 ? std::min(hw.leftJoystick.GetX(), hw.rightJoystick.GetX()) : std::max(hw.leftJoystick.GetX(), hw.rightJoystick.GetX());
+  //if you didn't already know you can twist the joysticks
+  //right = +, left = -
+  double joystickTwist;
+  if (hw.leftJoystick.GetTwist() < 0 || hw.rightJoystick.GetTwist() < 0) joystickTwist = std::min(hw.leftJoystick.GetTwist(), hw.rightJoystick.GetTwist());
+  else joystickTwist = std::max(hw.leftJoystick.GetTwist(), hw.rightJoystick.GetTwist());
   
+  double turningSensitivity = 0.128; //changes turning speed
+
   // deadzone
-  if (abs(joystickX) < 0.1) {
-    joystickX = 0.0;
-  }
+  if (abs(joystickX) < 0.1) joystickX = 0.0;
 
   double leftMotorPower = joystickY + joystickX * turningSensitivity;
   //clipping so it's betweeen -1 and 1
-  /*
   leftMotorPower = leftMotorPower > 1 ? 1 : leftMotorPower;
   leftMotorPower = leftMotorPower < -1 ? -1 : leftMotorPower;
-  */
-
-  if (leftMotorPower > 1) {
-    leftMotorPower = 1;
-  } else if (leftMotorPower < -1) {
-    leftMotorPower = -1;
-  }
-
+  
+  //right motors are negative since they face the opposite direction
   double rightMotorPower = -joystickY + joystickX * turningSensitivity;
   //clipping
-  /*
   rightMotorPower = rightMotorPower > 1 ? 1 : rightMotorPower;
   rightMotorPower = rightMotorPower < -1 ? -1 : rightMotorPower;
-  */
-
-  if (rightMotorPower > 1) {
-    rightMotorPower = 1;
-  } else if (rightMotorPower < -1) {
-    rightMotorPower = -1;
-  }
-
-
+  
+  //motor turning
   hw.frontLeftMotor.Set(ControlMode::PercentOutput, leftMotorPower);
   hw.backLeftMotor.Set(ControlMode::PercentOutput, leftMotorPower);
   hw.frontRightMotor.Set(ControlMode::PercentOutput, rightMotorPower);
   hw.backRightMotor.Set(ControlMode::PercentOutput, rightMotorPower);
+
+  //drifting - it's the button on the right on the face of the joysticks
+  if(hw.leftJoystick.GetRawButtonReleased(4) || hw.rightJoystick.GetRawButtonReleased(4)) cMode = !cMode;
+
+  //drift right
+  if(joystickTwist > 0 && cMode){
+    hw.frontLeftMotor.Set(ControlMode::PercentOutput, 1);
+    hw.backLeftMotor.Set(ControlMode::PercentOutput, 1);
+  }
+  //drift left
+  else if(joystickTwist < 0 && cMode){
+    hw.frontRightMotor.Set(ControlMode::PercentOutput, -1);
+    hw.backRightMotor.Set(ControlMode::PercentOutput, -1);
+  }
+
 }
 
 /**
--- marvin/src/main/cpp/hardware/HardwareMap.cpp
+++ marvin/src/main/cpp/hardware/HardwareMap.cpp
@@ -6,10 +6,14 @@
 
 class HardwareMap {
     public:
+        //motors
         TalonSRX frontLeftMotor = {HardwareIDs::fLM};
         TalonSRX backLeftMotor = {HardwareIDs::bLM};
         TalonSRX frontRightMotor = {HardwareIDs::fRM};
         TalonSRX backRightMotor = {HardwareIDs::bRM};
+        
+        //controllers
+        //these have to be set differently since they're going thru the frc namespace
         frc::Joystick leftJoystick{HardwareIDs::lJ};
         frc::Joystick rightJoystick{HardwareIDs::rJ};
         frc::XboxController xBox{HardwareIDs::xBox};
