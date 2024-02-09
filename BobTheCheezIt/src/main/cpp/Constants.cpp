ff0a9ab5dd9953516f0c11fcf6677e4dc08513b2
Jason Wang <91506829+Leek727@users.noreply.github.com>
2024-02-09 09:58:58 -0500

    removed past stuff

--- BobTheCheezIt/src/main/cpp/Constants.cpp
+++
@@ -1,8 +0,0 @@
-namespace constants {
-    static double TIMSCONSTANT = 3.131592653589;
-    static double ROBOTLENGTH = .3048;
-    static double ROBOTWIDTH = .5207;
-    static int slidingPWMWindowSize = 5;
-    static double GYROMAX = 3200.0;
-    static double ROTATION_OFFSET;
-}
