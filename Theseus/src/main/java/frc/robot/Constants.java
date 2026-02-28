15232e7e0dc8a499f0ec06e50027120024825fca
Vlliage <villagercool123@gmail.com>
2026-02-28 11:11:50 -0500

    Moved motor configs to Constants.java

--- Theseus/src/main/java/frc/robot/Constants.java
+++ Theseus/src/main/java/frc/robot/Constants.java
@@ -1,5 +1,10 @@
 package frc.robot;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
 public final class Constants {
   public static class OperatorConstants {
     //Controller Ports
@@ -36,6 +41,22 @@ public final class Constants {
 
     //Gyro ID
     public static final int kGyroID = 9;
+
+    //Climber ID
+    public static final int kClimberID = 10;
+    
+    //Turret device ids
+    //In parentheses because im too lazy
+    public static final int kTurretRotationID = (13);
+    public static final int kTurretHeightID = (14);
+    public static final int kTurretFlywheelID = (15);
+    public static final int kTurretIndexerID = (19);
+    public static final int kTurretFeedID = (20);
+
+    //intake device ids
+    public static final int kIntakeID = 16;
+    public static final int kIntakeFollowerID = 17;
+    public static final int kIntakeFWID = 18;
 
     //Rotation kP
     public static final double kFrontLeftP = 0.56;
@@ -104,7 +125,109 @@ public final class Constants {
     public static final double kIntakevelocity = 100;
     public static final double kIntakeAcceleration = 100;
 
-    //robot configs
+    //robot config
 
+  }
+  //Config CLasses
+  public static class TurretConfigs {
+    /** Configuration for the thingy that angles the shooter from {@link TurretConfigs}*/
+    public static final TalonFXConfiguration heightConfigs = new TalonFXConfiguration();
+    /** Configuration for the motor that rotates the turret from {@link TurretConfigs} */
+    public static final TalonFXConfiguration rotationConfigs = new TalonFXConfiguration();
+    /** Configuration for the flywheel from {@link TurretConfigs} */
+    public static final TalonFXConfiguration flyConfigs = new TalonFXConfiguration();
+    /** Configuration for the indexer from {@link TurretConfigs} */
+    public static final TalonFXConfiguration indexerConfigs = new TalonFXConfiguration();
+    public static final Double rotationRatio = 100.0;
+    public static final Double turretHeight = .508; 
+    public static final Double kmaxVelocity = 4.71;
+    public static final Double heightRatio = 5.0;
+    public static final Double smoothRotation = 0.0;
+    public static final Double smoothHeight = 0.0;
+    public static final Double rotationTao = .05;
+    public static final Double heightTao = .1;
+    static {
+      //This applies all the configuration
+        //basic motor configurations
+        rotationConfigs.MotorOutput.withInverted(InvertedValue.Clockwise_Positive);
+        rotationConfigs.MotorOutput.withNeutralMode(NeutralModeValue.Coast);
+        //current limits
+        rotationConfigs.CurrentLimits.withStatorCurrentLimit(40);
+        rotationConfigs.CurrentLimits.withStatorCurrentLimitEnable(true);
+        //motor limits
+        rotationConfigs.SoftwareLimitSwitch.ForwardSoftLimitThreshold = 130.0/360.0;
+        rotationConfigs.SoftwareLimitSwitch.ReverseSoftLimitThreshold = -220.0/360.0;
+        rotationConfigs.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
+        rotationConfigs.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+        rotationConfigs.Feedback.SensorToMechanismRatio = rotationRatio;
+        rotationConfigs.Slot0.kP = RobotConstants.kTurretRotationP;
+        rotationConfigs.Slot0.kI = RobotConstants.kTurretRotationI;
+        rotationConfigs.Slot0.kD = RobotConstants.kTurretRotationD;
+        rotationConfigs.MotionMagic.MotionMagicCruiseVelocity = RobotConstants.kTurretRotationVelocity;
+        rotationConfigs.MotionMagic.MotionMagicAcceleration = RobotConstants.kTurretRotationAcceleration;
+        rotationConfigs.serialize();
+
+        //height motors stuff
+        heightConfigs.MotorOutput.withInverted(InvertedValue.CounterClockwise_Positive);
+        heightConfigs.MotorOutput.withNeutralMode(NeutralModeValue.Coast);
+        heightConfigs.CurrentLimits.withStatorCurrentLimit(40);
+        heightConfigs.CurrentLimits.withStatorCurrentLimitEnable(true);
+        heightConfigs.SoftwareLimitSwitch.ForwardSoftLimitThreshold = 1.5;
+        heightConfigs.SoftwareLimitSwitch.ReverseSoftLimitThreshold = 0.0;
+        heightConfigs.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
+        heightConfigs.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+        heightConfigs.Feedback.SensorToMechanismRatio = heightRatio;
+        heightConfigs.Slot0.kP = RobotConstants.kTurretHeightP;
+        heightConfigs.Slot0.kI = RobotConstants.kTurretHeightI;
+        heightConfigs.Slot0.kD = RobotConstants.kTurretHeightD;
+        heightConfigs.MotionMagic.MotionMagicCruiseVelocity = RobotConstants.kTurretRotationVelocity;
+        heightConfigs.MotionMagic.MotionMagicAcceleration = RobotConstants.kTurretHeightAcceleration;
+
+        
+        //flywheel stuff
+        flyConfigs.MotorOutput.withInverted(InvertedValue.Clockwise_Positive);
+        flyConfigs.MotorOutput.withNeutralMode(NeutralModeValue.Coast);
+        flyConfigs.CurrentLimits.withStatorCurrentLimit(40);
+        flyConfigs.CurrentLimits.withStatorCurrentLimitEnable(true);
+        flyConfigs.serialize(); //save
+
+        //indexer stuff
+        indexerConfigs.MotorOutput.withInverted(InvertedValue.Clockwise_Positive);
+        indexerConfigs.MotorOutput.withNeutralMode(NeutralModeValue.Coast);
+        indexerConfigs.CurrentLimits.withStatorCurrentLimit(40);
+        indexerConfigs.CurrentLimits.withStatorCurrentLimitEnable(true);
+        indexerConfigs.serialize(); //save
+    }
+  }
+  public static class IntakeConstants {
+    /** Configs for the intake motors from {@link IntakeConstants} */
+    public static final TalonFXConfiguration intakeConf = new TalonFXConfiguration();
+    /** Configs for the intakeFW motor from {@link IntakeConstants} */
+    public static final TalonFXConfiguration intakeFWConf = new TalonFXConfiguration();
+    public static final double ActivatedPos = 1.45;
+    static {
+      //
+      intakeConf.MotorOutput.withInverted(InvertedValue.CounterClockwise_Positive);
+      intakeConf.MotorOutput.withNeutralMode(NeutralModeValue.Coast);
+      intakeConf.CurrentLimits.withStatorCurrentLimit(40);
+      intakeConf.CurrentLimits.withStatorCurrentLimitEnable(true);
+      //intakeOneConf.Feedback.SensorToMechanismRatio = 6.7;
+      intakeConf.Slot0.kP = RobotConstants.kIntakeP;
+      intakeConf.Slot0.kI = RobotConstants.kIntakeI;
+      intakeConf.Slot0.kD = RobotConstants.kIntakeD;
+      intakeConf.Slot0.kG = 0.3;
+      intakeConf.MotionMagic.MotionMagicCruiseVelocity = RobotConstants.kIntakevelocity;
+      intakeConf.MotionMagic.MotionMagicAcceleration = RobotConstants.kIntakeAcceleration;
+      intakeConf.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
+      intakeConf.serialize();
+
+      //Intake FW config
+      intakeFWConf.MotorOutput.withInverted(InvertedValue.CounterClockwise_Positive);
+      intakeFWConf.MotorOutput.withNeutralMode(NeutralModeValue.Coast);
+      intakeFWConf.CurrentLimits.withStatorCurrentLimit(40);
+      intakeFWConf.CurrentLimits.withStatorCurrentLimitEnable(true);
+      intakeFWConf.serialize();
+    }
+    
   }
 }
--- Theseus/src/main/java/frc/robot/subsystems/Intake.java
+++ Theseus/src/main/java/frc/robot/subsystems/Intake.java
@@ -6,56 +6,29 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.RobotConstants;
+import static frc.robot.Constants.IntakeConstants.*;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.signals.MotorAlignmentValue;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.signals.GravityTypeValue;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
 
 public class Intake extends SubsystemBase {
 
   private final TalonFX m_inkMot, intFWMot, m_inkMotFollower;
-  private final double ActivatedPos;
   private double target;
-  private final TalonFXConfiguration intakeOneConf, intakeFWConf;
   public Intake() {
 
     // defining + configuring motors
-    m_inkMot = new TalonFX(16);
-    m_inkMotFollower = new TalonFX(17);
-    intFWMot = new TalonFX(18);
-    intakeOneConf = new TalonFXConfiguration();
-    intakeFWConf = new TalonFXConfiguration();
-    ActivatedPos = 1.45;
+    m_inkMot = new TalonFX(RobotConstants.kIntakeID);
+    m_inkMotFollower = new TalonFX(RobotConstants.kIntakeFollowerID);
+    intFWMot = new TalonFX(RobotConstants.kIntakeFWID);
+
     target = 0;
+
     m_inkMot.setPosition(0);
-
-    intakeOneConf.MotorOutput.withInverted(InvertedValue.CounterClockwise_Positive);
-    intakeOneConf.MotorOutput.withNeutralMode(NeutralModeValue.Coast);
-    intakeOneConf.CurrentLimits.withStatorCurrentLimit(40);
-    intakeOneConf.CurrentLimits.withStatorCurrentLimitEnable(true);
-    //intakeOneConf.Feedback.SensorToMechanismRatio = 6.7;
-    intakeOneConf.Slot0.kP = RobotConstants.kIntakeP;
-    intakeOneConf.Slot0.kI = RobotConstants.kIntakeI;
-    intakeOneConf.Slot0.kD = RobotConstants.kIntakeD;
-    intakeOneConf.Slot0.kG = 0.3;
-    intakeOneConf.MotionMagic.MotionMagicCruiseVelocity = RobotConstants.kIntakevelocity;
-    intakeOneConf.MotionMagic.MotionMagicAcceleration = RobotConstants.kIntakeAcceleration;
-    intakeOneConf.Slot0.GravityType = GravityTypeValue.Arm_Cosine;
-
-    intakeOneConf.serialize();
-    m_inkMot.getConfigurator().apply(intakeOneConf);
+    m_inkMot.getConfigurator().apply(intakeConf);
     m_inkMotFollower.setControl(new Follower(m_inkMot.getDeviceID(), MotorAlignmentValue.Opposed));
-
-    intakeFWConf.MotorOutput.withInverted(InvertedValue.CounterClockwise_Positive);
-    intakeFWConf.MotorOutput.withNeutralMode(NeutralModeValue.Coast);
-    intakeFWConf.CurrentLimits.withStatorCurrentLimit(40);
-    intakeFWConf.CurrentLimits.withStatorCurrentLimitEnable(true);
-    intakeFWConf.serialize();
     intFWMot.getConfigurator().apply(intakeFWConf);
   }
 
