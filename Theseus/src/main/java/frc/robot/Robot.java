7889241a45c30a7c116443690536be256b47d3e2
Doug Horner <horner@mieweb.com>
2026-03-14 22:06:10 -0400

    feat: implement log pruning functionality to manage log sessions on roboRIO

--- Theseus/src/main/java/frc/robot/Robot.java
+++ Theseus/src/main/java/frc/robot/Robot.java
@@ -38,6 +38,10 @@
 
 package frc.robot;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
+
 import com.ctre.phoenix6.HootAutoReplay;
 
 import org.littletonrobotics.junction.LoggedRobot;
@@ -56,6 +60,10 @@ public class Robot extends LoggedRobot {
     private final RobotContainer m_robotContainer;
     private final Field2d field;
 
+    /** Maximum number of log sessions to keep on the roboRIO. */
+    private static final int MAX_LOG_SESSIONS = 20;
+    private static final File LOG_DIR = new File("/home/lvuser/logs");
+
     /* log and replay timestamp and joystick data */
     private final HootAutoReplay m_timeAndJoystickReplay = new HootAutoReplay()
         .withTimestampReplay()
@@ -66,7 +74,7 @@ public class Robot extends LoggedRobot {
         Logger.recordMetadata("TeamNumber", "4982");
 
         if (isReal()) {
-            Logger.addDataReceiver(new WPILOGWriter());
+            Logger.addDataReceiver(new WPILOGWriter("/home/lvuser/logs"));
             Logger.addDataReceiver(new NT4Publisher());
         } else {
             Logger.addDataReceiver(new NT4Publisher());
@@ -80,7 +88,60 @@ public class Robot extends LoggedRobot {
 
     @Override
     public void robotInit() {
-        
+        if (isReal()) {
+            pruneOldLogs();
+        }
+    }
+
+    /**
+     * Deletes the oldest log directories and .revlog files so only the most
+     * recent {@link #MAX_LOG_SESSIONS} of each are kept on the roboRIO.
+     */
+    private void pruneOldLogs() {
+        if (!LOG_DIR.isDirectory()) return;
+
+        // Prune old session directories (CTRE hoot / AdvantageKit wpilog)
+        File[] dirs = LOG_DIR.listFiles(File::isDirectory);
+        if (dirs != null && dirs.length > MAX_LOG_SESSIONS) {
+            Arrays.sort(dirs, Comparator.comparing(File::getName));
+            int toDelete = dirs.length - MAX_LOG_SESSIONS;
+            int deleted = 0;
+            for (int i = 0; i < toDelete; i++) {
+                if (deleteRecursive(dirs[i])) deleted++;
+            }
+            System.out.println("[LogRotate] Pruned " + deleted + " old log sessions, kept " + MAX_LOG_SESSIONS);
+        }
+
+        // Prune old .wpilog files
+        pruneFilesByExtension(".wpilog");
+
+        // Prune old REV .revlog files
+        pruneFilesByExtension(".revlog");
+    }
+
+    private void pruneFilesByExtension(String ext) {
+        File[] files = LOG_DIR.listFiles((dir, name) -> name.endsWith(ext));
+        if (files != null && files.length > MAX_LOG_SESSIONS) {
+            Arrays.sort(files, Comparator.comparing(File::getName));
+            int toDelete = files.length - MAX_LOG_SESSIONS;
+            int deleted = 0;
+            for (int i = 0; i < toDelete; i++) {
+                if (files[i].delete()) deleted++;
+            }
+            System.out.println("[LogRotate] Pruned " + deleted + " old " + ext + " files, kept " + MAX_LOG_SESSIONS);
+        }
+    }
+
+    private boolean deleteRecursive(File file) {
+        if (file.isDirectory()) {
+            File[] children = file.listFiles();
+            if (children != null) {
+                for (File child : children) {
+                    deleteRecursive(child);
+                }
+            }
+        }
+        return file.delete();
     }
 
     @Override
